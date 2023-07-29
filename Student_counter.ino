653a34ea3ed65563bb015ed18d461fc3b502e549
Ishola Abdulkadir <31169496+DFOMULAR@users.noreply.github.com>
2023-07-29 21:07:45 +0100

    Student_counter.ino

--- Student_counter.ino
+++ Student_counter.ino
@@ -1,3 +1,34 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <Firebase_ESP_Client.h>
+
+//Provide the token generation process info.
+#include "addons/TokenHelper.h"
+//Provide the RTDB payload printing info and other helper functions.
+#include "addons/RTDBHelper.h"
+
+// Insert your network credentials
+#define WIFI_SSID "DoESLiverpool"
+#define WIFI_PASSWORD "decafbads00"
+
+// Insert Firebase project API Key
+#define API_KEY "ByiecarDXxxLsznLQSB4Vv5MQxSiSPVx6q11iNTV"
+
+// Insert RTDB URLefine the RTDB URL */
+#define DATABASE_URL "https://iotvoice-9d398-default-rtdb.firebaseio.com/" 
+
+//Define Firebase Data object
+FirebaseData fbdo;
+
+FirebaseAuth auth;
+FirebaseConfig config;
+
+unsigned long sendDataPrevMillis = 0;
+int count = 0;
+bool signupOK = false;
+
+
+//ultra_def
 const int t1 = 5;
 const int e1= 18;
 
@@ -15,16 +46,52 @@ long duration;
 float distanceCm;
 float distanceInch;
 
-void setup() {
-  Serial.begin(115200); // Starts the serial communication
-  pinMode(t1, OUTPUT); // Sets the trigPin as an Output
+
+
+
+void setup(){
+  Serial.begin(115200);
+  pinMode(t1, OUTPUT); // Sets the trigpin1 as an Output
   pinMode(e1, INPUT); // Sets the echoPin as an Input4
   pinMode(t2, OUTPUT); // Sets the trigPin as an Output
   pinMode(e2, INPUT); // Sets the echoPin as an Input
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  /* Assign the api key (required) */
+  config.api_key = API_KEY;
+
+  /* Assign the RTDB URL (required) */
+  config.database_url = DATABASE_URL;
+
+  /* Sign up */
+  if (Firebase.signUp(&config, &auth, "", "")){
+    Serial.println("ok");
+    signupOK = true;
+  }
+  else{
+    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+  }
+
+  /* Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+  
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
 }
 
-void loop() {
-d1 =getdistance(t1,e1);
+void loop(){
+
+  d1 =getdistance(t1,e1);
 d2 =getdistance(t2,d2);
 
 if(d1<5){
@@ -42,24 +109,51 @@ if(student>0){
 
 }
 
-  
+
+  if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0)){
+    sendDataPrevMillis = millis();
+    // Write an Int number on the database path test/int
+    if (Firebase.RTDB.setInt(&fbdo, "Incubator/No", student)){
+      Serial.println("PASSED");
+      Serial.println("PATH: " + fbdo.dataPath());
+      Serial.println("TYPE: " + fbdo.dataType());
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+    //count++;
+    
+    // Write an Float number on the database path test/float
+    if (Firebase.RTDB.setFloat(&fbdo, "test/float", 0.01 + random(0,100))){
+      Serial.println("PASSED");
+      Serial.println("PATH: " + fbdo.dataPath());
+      Serial.println("TYPE: " + fbdo.dataType());
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+  }
 }
 
 
-float getdistance(int trigPin, int echopin){
- int trigpin;
- int echopin;
+
+
+
+
+float getdistance(int t, int e){
 
     // Clears the trigPin
-  digitalWrite(trigPin, LOW);
+  digitalWrite(t, LOW);
   delayMicroseconds(2);
   // Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(trigPin, HIGH);
+  digitalWrite(t, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
+  digitalWrite(t, LOW);
   
   // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, HIGH);
+  duration = pulseIn(e, HIGH);
   
   // Calculate the distance
   distanceCm = duration * SOUND_SPEED/2;
